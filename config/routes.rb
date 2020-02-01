9fe5e40b561b5d907ba978d82962c61071700a9d
dot <muharremmertozturk@gmail.com>
2020-02-01 14:30:40 +0300

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index' # tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action.
+  resources :articles #Rails provides a resources method which can be used to declare a standard REST resource.
+
+
+  root 'welcome#index' #This was created earlier when you ran the controller generator (rails generate controller Welcome index). / tells Rails to map requests to the root of the application to the welcome controller's index action
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
